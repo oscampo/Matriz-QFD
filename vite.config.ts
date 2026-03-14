9ccca41949a50b6a17e7f6ffa261c272ef6b49c3
Oscar Campo <oscampo@hotmail.com>
2026-03-14 19:37:27 +0000

    modified:   .env.example

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/Matriz-QFD/',
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
