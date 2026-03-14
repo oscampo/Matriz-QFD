91fe4603b7a74d2831044924d5f59c936c1413c5
Oscar Campo <oscampo@hotmail.com>
2026-03-14 17:46:10 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // <--- IMPORTANTE
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Matriz-QFD/', 
-    plugins: [react()],
+    base: '/Matriz-QFD/',
+    plugins: [
+      react(),
+      tailwindcss(), // <--- IMPORTANTE
+    ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(process.env.GEMINI_API_KEY || env.GEMINI_API_KEY || ''),
+    },
+    // Añadimos esto para asegurar que el CSS se extraiga correctamente
+    css: {
+      postcss: './postcss.config.js', 
     }
   };
 });
